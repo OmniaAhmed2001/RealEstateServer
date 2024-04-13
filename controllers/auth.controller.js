11a40db4107061b64f22cceb36671e5dcdf125d0
loubnakamalrady <loubnakamal1998@hotmail.com>
2024-04-14 01:48:20 +0200

    errorHandler status update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -19,9 +19,21 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404, "User not found!"));
+    if (!validUser)
+      return next(
+        errorHandler(
+          404,
+          "Incorrect email address or password, please try again"
+        )
+      );
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+    if (!validPassword)
+      return next(
+        errorHandler(
+          401,
+          "Incorrect email address or password, please try again"
+        )
+      );
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
     res
