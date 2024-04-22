3033613926c36299c32bc23e4617918c6b8998d7
OmniaAhmed2001 <mazenerashidy@gmail.com>
2024-04-22 20:21:31 +0200

    feat: :sparkles: Add and handle delete account api route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,31 +7,47 @@ export const test = (req, res) => {
 };
 
 export const updateUser = async (req, res, next) => {
-  console.log("pls work")
-  if(req.user.id !== req.params.id) return next(errorHandler(401, "you can only update your account!"))
+  console.log("pls work");
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "you can only update your account!"));
 
   try {
-    if(req.body.password) {
-      req.body.password = bcryptjs.hashSync(req.body.password, 10)
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
 
-    const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-      $set: {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        avatar: req.body.avatar
-      }
-    }, {new:true})
-    console.log("yes")
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+    console.log("yes");
 
-    const { password, ...rest } = updatedUser._doc
-    res.status(200).json(rest)
-
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
   } catch (error) {
-    next(error)
+    next(error);
   }
 
   const { username, password, email } = req.body;
+};
 
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "you can only delete your own account! "));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie('access_token');
+    res.status(200).json("User has been deleted");
+  } catch (error) {
+    next(error)
+  }
 };
