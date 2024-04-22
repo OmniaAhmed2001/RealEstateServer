0fd2091ebc3839b57d093b5f4ab00030212e88e4
OmniaAhmed2001 <mazenerashidy@gmail.com>
2024-04-22 21:10:15 +0200

    feat: :sparkles: Add and Handle signOut api route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { google, signin, signup } from "../controllers/auth.controller.js";
+import { google, signOut, signin, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/sign-up", signup);
 router.post("/sign-in", signin);
 router.post("/google", google);
+router.get("/sign-out", signOut);
 
 export default router;
--- routes/user.route.js
+++ routes/user.route.js
@@ -8,4 +8,5 @@ router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser)
 
+
 export default router;
