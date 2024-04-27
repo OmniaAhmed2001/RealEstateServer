fe9918a3cdfde2d53df36eef23bbfdf1ea769020
OmniaAhmed2001 <mazenerashidy@gmail.com>
2024-04-27 23:14:47 +0300

    feat: :bug: add cross origin feature

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 
 dotenv.config();
 
@@ -25,6 +25,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.use(cookieParser());
 
