6ccbaca738b38bda04834dbbb6c3e0d743b311f3
OmniaAhmed2001 <mazenerashidy@gmail.com>
2024-04-30 01:12:41 +0300

    add get user by Id api functionality

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,6 +5,7 @@ import {
   test,
   updateUser,
   getUserListings,
+  getUser,
 } from "../controllers/user.controller.js";
 
 import { verifyToken } from "../utils/verifyUser.js";
@@ -13,9 +14,8 @@ const router = express.Router();
 
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
-
 router.get("/listings/:id", verifyToken, getUserListings);
-
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/:id", verifyToken, getUser);
 
 export default router;
